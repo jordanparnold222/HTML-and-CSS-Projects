88c34ec8b935ec6b15fdd2c73d2741ddeb44b49d
jordanparnold222 <42490695+jordanparnold222@users.noreply.github.com>
2021-06-22 18:19:49 -0700

    One Page lightbox submission

--- One-Page Website/JS/one_page_website.js
+++ One-Page Website/JS/one_page_website.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <header></header>
-    <main></main>
-    <footer></footer>
-  </body>
-</html>
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
